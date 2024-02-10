df48e7af7133f67aad5ada302b8bff74342bd4e2
Tushar Anand <tushar.anand@provana.com>
2024-02-10 23:18:45 +0530

    Added unit tests for Data

--- AdvancedLINQ/AdvancedLINQ/Startup.cs
+++ AdvancedLINQ/AdvancedLINQ/Startup.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataLayer;
+
+namespace AdvancedLINQ
+{
+    class Startup
+    {
+        public static void Main(string[] args)
+        {
+            //var employees = Data.GetEmployees();
+            //var departments = Data.GetDepartments();
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ AdvancedLINQ/TestProject/DataTest.cs
@@ -0,0 +1,43 @@
+﻿using DataLayer;
+using DataLayer.PretendCompanyModals;
+using DataLayer.PretendCompanyModels;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class DataTest
+    {
+        [Fact] 
+        public void GetEmployeesList_Data()
+        {
+            //Arrange
+            int employeeCount_Expected = 8;
+
+            //Act
+            List<EmployeeModel> employeesList_Actual = Data.GetEmployees();
+
+            //Assert
+            employeesList_Actual.Should().NotBeNull();
+            employeesList_Actual.Count.Should().Be(employeeCount_Expected);
+        }
+
+        [Fact] 
+        public void GetDepartmentList_Data()
+        {
+            //Arrange
+            int employeeCount_Expected = 4;
+
+            //Act
+            List<DepartmentModel> departmentList_Actual = Data.GetDepartments();
+
+            //Assert
+            departmentList_Actual.Should().NotBeNull();
+            departmentList_Actual.Count.Should().Be(employeeCount_Expected);
+        }
+    }
+}
